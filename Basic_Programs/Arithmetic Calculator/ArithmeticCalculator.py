558a25cf5424df4227bc7baef375644cf8b4aa73
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-17 10:41:19 +0530

    Add files via upload

---
+++ Basic_Programs/Arithmetic Calculator/ArithmeticCalculator.py
@@ -0,0 +1,16 @@
+number_1 = int(input("Enter the number1: "))
+number_2 = int(input("Enter the number2: "))
+operator = input("Enter the operator: ")
+
+if operator == '+':
+    print(f"The Sum of {number_1} and {number_2} is {number_1 + number_2}")
+elif operator == '-':
+    print(f"The Difference between {number_1} and {number_2} is {number_1 - number_2}")
+elif operator == '*':
+    print(f"The Product of {number_1} and {number_2} is {number_1 * number_2}")
+elif operator == '/':
+    print(f"The Division of {number_1} by {number_2} is {number_1 / number_2}")
+
+
+
+

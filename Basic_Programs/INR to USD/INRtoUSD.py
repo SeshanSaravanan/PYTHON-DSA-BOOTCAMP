688f835fec861bd4be9926bbbe3661f677128908
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-17 10:43:08 +0530

    Add files via upload

---
+++ Basic_Programs/INR to USD/INRtoUSD.py
@@ -0,0 +1,5 @@
+money_in_rupees = int(input("Enter the amount (in rupees): "))
+one_dollar = int(input("Value of one dollar in rupees: "))
+
+money_in_dollars = money_in_rupees / one_dollar
+print(f"Value of amount (in dollars) {money_in_dollars}.")

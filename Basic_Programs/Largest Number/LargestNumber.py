1c4bdd83fb126cd825a003573e8cd2351fe0e501
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-17 10:43:51 +0530

    Add files via upload

---
+++ Basic_Programs/Largest Number/LargestNumber.py
@@ -0,0 +1,7 @@
+number_1 = int(input("Enter the number1: "))
+number_2 = int(input("Enter the number2: "))
+
+if number_1 > number_2:
+    print(f"{number_1} is the largest number")
+else:
+    print(f"{number_2} is the largest number")
